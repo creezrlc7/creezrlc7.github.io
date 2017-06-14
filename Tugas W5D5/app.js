7c91af3c2ffaf6a238d6ed6e5024fe4776c9ab70
Christian <christian.rlc7@gmail.com>
2017-06-14 16:45:25 +0700

    Upload Tugas W5D5

---
+++ Tugas W5D5/app.js
@@ -0,0 +1,90 @@
+/*
+GAME RULES:
+
+- Game dimainkan oleh 2 player secara bergiliran.
+- Setiap giliran, player dapat melempar dadu sebanyak mungkin. Setiap hasil dadu ditambahkan ke ROUND score.
+- Tapi jika player mendapat hasil 1, player akan kehilangan semua score pada ROUND score, dan giliran pindah ke player selanjutnya.
+- Player dapat memilih HOLD, yang artinya tottal ROUND score ditambahkan ke GLOBAL score. Selanjutnya giliran akan pindah ke player selanjutnya.
+- Player yang terlebih dahulu mendapat score 100 pada GLOBAL score akan menang.
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        scores[activePlayer] += roundScore;
+
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
